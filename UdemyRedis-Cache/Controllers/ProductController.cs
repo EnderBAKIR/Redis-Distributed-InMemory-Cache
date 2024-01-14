fac13f0431352e5cebc71fe2b2c9dcf083e6f597
Ender Bakır <enderyx_123@hotmail.com>
2024-01-15 00:28:39 +0300

    project created

---
+++ UdemyRedis-Cache/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace UdemyRedis_Cache.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UdemyRedis-Cache/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UdemyRedis_Cache.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
