408cd33ef3881351b0dacd17e75a2338692def53
Ender Bakır <enderyx_123@hotmail.com>
2024-01-17 22:31:56 +0300

    InMemoryCache With Product Model

--- UdemyRedis-Cache/Controllers/ProductController.cs
+++ UdemyRedis-Cache/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
+using UdemyRedis_Cache.Models;
 
 namespace UdemyRedis_Cache.Controllers
 {
@@ -14,6 +15,37 @@ namespace UdemyRedis_Cache.Controllers
 
         public IActionResult Index()
         {
+
+
+
+
+
+
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);//Memoryden silinme süresini ayarla//Set Delete Time From Memory
+            options.Priority = CacheItemPriority.High;
+
+
+
+            options.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _memoryCache.Set("callback", $"{key}->{value} => sebep:{reason}");
+            });
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
+
+            Product product = new Product {Id = 1 , Name="Araba" , Price=1900 };
+            _memoryCache.Set<Product>("product:1" , product );
+
+            return View();
+        }
+        public IActionResult Show()
+        {
+
+            _memoryCache.TryGetValue("zaman", out string zamancache);
+            _memoryCache.TryGetValue("callback", out string callback);
+            ViewBag.zaman = zamancache;
+            ViewBag.callback = callback;
+            ViewBag.product = _memoryCache.Get<Product>("product:1");
             return View();
         }
     }
---
+++ UdemyRedis-Cache/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace UdemyRedis_Cache.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+    }
+}
