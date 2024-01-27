ebd4611ef5e944027082ae0f3ad1427541f44fb6
Ender Bakır <enderyx_123@hotmail.com>
2024-01-27 13:35:33 +0300

    Json Serializer And Binary Data Caching
    Caching binary data in the Product Controller and caching complex data with Json Serializers.

--- IDistributedCacheRedisWeb.App/Controllers/ProductsController.cs
+++ IDistributedCacheRedisWeb.App/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace IDistributedCacheRedisWeb.App.Controllers
 {
@@ -26,26 +27,56 @@ namespace IDistributedCacheRedisWeb.App.Controllers
             Products products = new Products {Id=1 , Name="Araba" , Price=1000 };
 
             string jsonproduct =JsonConvert.SerializeObject(products);
-           await _distributedCache.SetStringAsync("products:1", jsonproduct , cacheOptions);
+            Byte[] byteproduct = Encoding.UTF8.GetBytes(jsonproduct);
+
+            _distributedCache.Set("product:1", byteproduct);
+
+           //await _distributedCache.SetStringAsync("products:1", jsonproduct , cacheOptions);
 
             return View();
         }
+
         public IActionResult show()
         {
 
             //string name = _distributedCache.GetString("name");
             //ViewBag.Name = name;
-           
+
+            Byte[] byteProduct = _distributedCache.Get("product:1");
+            string jsonproduct = Encoding.UTF8.GetString(byteProduct);
             
+            Products p = JsonConvert.DeserializeObject<Products>(jsonproduct);
+            ViewBag.product = p;
             return View();
 
 
         }
+
         public IActionResult Remove()
         {
             //_distributedCache.Remove("name");
 
             return View();
+        }
+
+        public IActionResult ImageCache()
+        {
+
+            String path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/download.jpg");
+
+            byte[] imageByte = System.IO.File.ReadAllBytes(path);
+
+            _distributedCache.Set("resim", imageByte);
+
+            return View();
+        }
+
+        public IActionResult ImageUrl()
+        {
+            byte[] resimbyte = _distributedCache.Get("resim");
+            return File(resimbyte, "image/jpg");
+            
+            
         }
     }
 }
